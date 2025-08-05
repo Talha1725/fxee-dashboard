ad5728a079700ec7d6921354527623b2b57556f4
talha1725 <talhanoor1001@gmail.com>
2025-08-05 17:39:56 +0500

    Refactor BrokerPlan component to enhance styling and improve readability; adjust class structure for better layout and maintain dark mode support.

--- src/components/features/onboard/broker/BrokerPlan.tsx
+++ src/components/features/onboard/broker/BrokerPlan.tsx
@@ -10,12 +10,21 @@ import { useTheme } from "@/lib/contexts/ThemeContext";
 export default function BrokerPlan() {
   const { theme } = useTheme();
   return (
-    <OnboardCardContainer className={`w-full max-w-[420px] gap-4 ${theme === "dark" ? "bg-dark-radial-gradient" : "bg-light-green-gradient border-green-gradient"} mx-auto z-50`}>
+    <OnboardCardContainer
+      className={`w-full max-w-[420px] gap-4 ${
+        theme === "dark"
+          ? "bg-dark-radial-gradient"
+          : "bg-light-green-gradient border-green-gradient"
+      } mx-auto z-50`}
+    >
       <BrokerSelection />
       <BrokerInput />
       <Button variant="fancy" className="w-full">
         Connect
       </Button>
+      {/* <div className="absolute bottom-0 right-[16px] left-[16px]">
+          <div className="h-[4px] w-full rounded-tr-lg rounded-tl-lg overflow-hidden bg-popular-gradient"></div>
+        </div> */}
     </OnboardCardContainer>
   );
 }
