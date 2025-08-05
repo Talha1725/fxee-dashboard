783cec3176a8a1027e73364ee98570fcb93d5b6d
talha1725 <talhanoor1001@gmail.com>
2025-08-05 18:52:41 +0500

    Remove package-lock.json, add Vercel configuration, and enhance styling in sign components; update global CSS for font support, adjust layout in sign forms, and implement navigation for password reset functionality.

--- src/components/features/sign/SignForgetPasswordForm.tsx
+++ src/components/features/sign/SignForgetPasswordForm.tsx
@@ -1,16 +1,20 @@
+"use client";
+
 import SignFormHeader from "@/components/features/sign/SignFormHeader";
 import SignSocialButtons from "@/components/features/sign/SignSocialButtons";
 import SignOrDivider from "@/components/features/sign/SignOrDivider";
 import SignForgetPasswordInputs from "@/components/features/sign/SignForgetPasswordInputs";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function SignForgetPasswordForm() {
+  const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center self-stretch flex-[1_0_0]">
       <div className="sm:w-[420px] flex flex-col items-end gap-6">
         <SignFormHeader isSignup={false} isForgotPassword={true} isResetPassword={false} />
         <SignForgetPasswordInputs />
-        <Button variant="fancy" className="flex-[1_0_0] self-stretch">
+        <Button onClick={() => router.push("/reset-password")} variant="fancy" className="flex-[1_0_0] self-stretch">
           Send Reset Link
         </Button>
       </div>
