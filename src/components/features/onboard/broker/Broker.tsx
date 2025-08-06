9c228a802a3dd66a968e7b7525ca1eed4e048f3f
talha1725 <talhanoor1001@gmail.com>
2025-08-06 11:28:27 +0500

    Add QR code functionality to Broker component; enhance styling and layout in BrokerPlan and BrokerInput components, and update global CSS for font support.

--- src/components/features/onboard/broker/Broker.tsx
+++ src/components/features/onboard/broker/Broker.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 
 import OnboardContentContainer from "@/components/features/onboard/OnboardContentContainer";
 import OnboardContentHeader from "@/components/features/onboard/OnboardContentHeader";
 import BrokerPlan from "@/components/features/onboard/broker/BrokerPlan";
 
 export default function Broker() {
+  const [qrSection, setQrSection] = useState(false);
   return (
     <OnboardContentContainer>
       <OnboardContentHeader
         title="Payment"
-        description="Connect Broker Account"
+        description={`${qrSection ? "Fund Account with Crypto" : "Broker Account"}`}
       />
-      <BrokerPlan />
+      <BrokerPlan qrSection={qrSection} setQrSection={setQrSection} />
     </OnboardContentContainer>
   );
 }
