3472d081ea9af6602aca94fbe4585a3032167c2f
KashanGK <kashanrk12@gmail.com>
2025-08-28 15:55:22 +0500

    types added

--- src/components/features/protected/ai-engine/status/AIEngineStatus.tsx
+++ src/components/features/protected/ai-engine/status/AIEngineStatus.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import DashboardACP from "../../dashboard/ACP/DashboardACP";
 import AIEngineStatusTGS from "./AIEngineStatusTGS";
+import type { ProposedTrade } from "@/types/redux";
 
 export default function AIEngineStatus() {
   const [activeTab, setActiveTab] = useState("best_trade");
-  const [bestTrade, setBestTrade] = useState(null);
-  const [customAnalysis, setCustomAnalysis] = useState(null);
+  const [bestTrade, setBestTrade] = useState<ProposedTrade | null>(null);
+  const [customAnalysis, setCustomAnalysis] = useState<any>(null);
 
   return (
     <div className="flex flex-col lg:flex-row items-start gap-5 self-stretch w-full min-h-[657px]">
