77853242f30314703867f49c13779f569c73ac78
malir <maliramzan314@gmail.com>
2025-08-28 18:16:25 +0500

    - Fix Public Navbar

--- src/components/features/landing/landingNavbar/LandingNavbarLink.tsx
+++ src/components/features/landing/landingNavbar/LandingNavbarLink.tsx
@@ -15,7 +15,7 @@ export default function LandingNavbarLink({
 }: LandingNavbarLinkProps) {
   return (
     <Link href={href} prefetch={true}>
-      <Text16 className="px-2.5 py-1.5 rounded-md text-white/40 font-satoshi text-base font-[400] hover:text-white/90 hover:bg-white/10 transition-colors shrink-0">
+      <Text16 className="px-2.5 py-1.5 rounded-md lg:!text-white/40 font-satoshi text-base font-[400] hover:text-white/90 transition-colors shrink-0">
         {children}
       </Text16>
     </Link>
