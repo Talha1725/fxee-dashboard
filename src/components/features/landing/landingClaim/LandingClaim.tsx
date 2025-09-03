1e72b9c6719f9582b63f438a9b1404488c68d410
malir <maliramzan314@gmail.com>
2025-09-03 19:38:29 +0500

    - Claim section fixed

--- src/components/features/landing/landingClaim/LandingClaim.tsx
+++ src/components/features/landing/landingClaim/LandingClaim.tsx
@@ -6,20 +6,17 @@ import Image from "next/image";
 import LandingMax1440Container from "@/components/features/landing/LandingMax1440Container";
 import LandingClaimHead from "@/components/features/landing/landingClaim/LandingClaimHead";
 import LandingClaimItem from "@/components/features/landing/landingClaim/LandingClaimItem";
-import LandingButton from "@/components/features/landing/LandingButton";
 import {
   IconBTCShield,
-  IconEthWhite,
-  IconLandingBtn2,
   IconSecurity,
   IconZapLinear,
 } from "@/components/ui/icon";
 
-import LandingClaimCoin1 from "@/public/images/landing-claim-coin1.png";
-import LandingClaimCoin2 from "@/public/images/landing-claim-coin2.png";
-import LandingClaimCoin3 from "@/public/images/landing-claim-coin3.png";
-import LandingClaimCoin3Mobile from "@/public/images/coin3-mobile.svg";
-import LandingClaimCoin4 from "@/public/images/landing-claim-coin4.png";
+
+import LandingClaimCoin3 from "@/public/images/coin-big.svg";
+import LandingClaimCoin3Mobile from "@/public/images/small-coin.svg";
+import claim from "@/public/images/claim.svg";
+import { Button } from "@/components/ui/button";
 
 export default function LandingClaim() {
   const [isMobile, setIsMobile] = useState(false);
@@ -35,28 +32,15 @@ export default function LandingClaim() {
     return () => window.removeEventListener('resize', checkScreenSize);
   }, []);
   return (
-    <div className="relative bg-white overflow-hidden">
-      <Image
-        src={LandingClaimCoin1}
-        alt="Landing Claim Coin 1"
-        className="absolute top-0 right-[10px] lg:right-[215px] w-[138px] h-[131px] lg:w-[244px] lg:h-[210px]"
-      />
-      <Image
-        src={LandingClaimCoin2}
-        alt="Landing Claim Coin 2"
-        className="absolute left-0 top-[118px] w-[80px] h-[132px] lg:w-[160px] lg:h-[231px]"
-      />
+    <div className="relative claim-gradient overflow-hidden border-gradient-blue-green border-l-0 border-r-0">
+     
       <Image
         src={isMobile ? LandingClaimCoin3Mobile : LandingClaimCoin3}
         alt="Landing Claim Coin 3"
-        className="absolute left-[0px] md:left-[10px] lg:left-[121px] bottom-[170px] md:bottom-0 w-[100px] h-[auto] md:w-[160px] md:h-[156px] lg:w-[244px] lg:h-[231px]"
+        className="absolute right-0 md:right-[10%] lg:right-[27%] bottom-[10px] md:bottom-0 w-[120px] h-[auto] md:w-[160px] md:h-[156px] lg:w-[192px] lg:h-[161px]"
       />
-      <Image
-        src={LandingClaimCoin4}
-        alt="Landing Claim Coin 4"
-        className="absolute -right-[5px] lg:right-[18px] bottom-[-30px] lg:bottom-[65px] w-[100px] h-[auto] md:w-[160px] md:h-[174px] lg:w-[244px] lg:h-[274px]"
-      />
-      <LandingMax1440Container className="py-25 gap-12.5 sm:gap-[50px]">
+     
+      <LandingMax1440Container className="py-15 gap-12.5 sm:gap-[50px]">
         <LandingClaimHead />
         <div className="flex flex-col md:flex-row gap-2.5 md:gap-0 justify-center items-center w-full lg:w-[850px]">
           <LandingClaimItem
@@ -79,17 +63,15 @@ export default function LandingClaim() {
           />
         </div>
         <div className="flex flex-col items-center gap-2.5">
-          <LandingButton
-            color="white"
-            icon={
-              <React.Fragment>
-                <IconLandingBtn2 className="absolute -z-1 w-full h-full top-0 left-0" />
-                <IconEthWhite className="shrink-0 w-[24px] h-[32px]" />
-              </React.Fragment>
-            }
-            text="Claim Your $100 Bonus in ETH"
-          />
-          <p className="text-black/40 text-center text-[14px] sm:text-[16px] font-regular font-medium tracking-[-0.32px]">
+          <Button
+            variant="white"
+            className="font-satoshi-medium flex items-center justify-center gap-3 px-4"
+          ><Image src={claim} alt="Claim" className="w-[24px] h-[24px]" />
+          <p>
+
+          Claim Your $100          </p>
+          </Button>
+          <p className="text-white/40 text-center text-[14px] sm:text-[16px] font-regular font-medium tracking-[-0.32px]">
             *Terms and conditions apply
           </p>
         </div>
--- src/components/features/landing/landingClaim/LandingClaimHead.tsx
+++ src/components/features/landing/landingClaim/LandingClaimHead.tsx
@@ -10,10 +10,10 @@ import { fadeInUpView } from "@/lib/motion-variants";
 export default function LandingClaimHead() {
   return (
     <motion.div className="flex flex-col items-center gap-5" {...fadeInUpView}>
-      <LandingTitle className="w-full lg:w-[950px] text-center tracking-[-3.6px] leading-[120%] text-landing-title-black-gradient">
+      <LandingTitle className="text-[48px] lg:text-[56px] w-full lg:max-w-[600px] text-center tracking-[-3.6px] leading-[120%] font-satoshi-medium text-white">
         Get Started with FXEE — Earn $100 in ETH Instantly
       </LandingTitle>
-      <LandingDescription className="text-center !text-black/40 w-full lg:w-[700px]">
+      <LandingDescription className="text-center !text-white/30 w-full lg:w-[700px]">
         Join FXEE Today and Receive $100 Worth of Ethereum Deposited into Your
         Trading Account
       </LandingDescription>
