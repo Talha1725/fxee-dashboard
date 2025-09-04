2529c5b00472ca949a1610068f8cd918013a9b84
sabasiddique1 <sabasiddiqdev@gmail.com>
2025-09-04 21:19:34 +0500

    feat:minor UI polishing

--- src/components/features/protected/dashboard/status/detail/DashboardStatusDetailAI.tsx
+++ src/components/features/protected/dashboard/status/detail/DashboardStatusDetailAI.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-import DashboardStatusDetailCardContainer from "./DashboardStatusDetailCardContainer";
-import HomeAIChat from "../../../home/homeNewsAI/HomeAIChat";
-import HomeAIMessageHead from "../../../home/homeNewsAI/HomeAIMessageHead";
-import HomeAIMessageBody from "../../../home/homeNewsAI/HomeAIMessageBody";
 import DashboardChatbot from "../../chatbot/DashboardChatbot";
 
 export default function DashboardStatusDetail() {
