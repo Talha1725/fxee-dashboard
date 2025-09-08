db2637597c5d968b4e7afca3c2b60646052ddfb0
sabasiddique1 <sabasiddiqdev@gmail.com>
2025-09-09 01:35:07 +0500

    feat: news impact add

--- src/components/features/protected/home/Home.tsx
+++ src/components/features/protected/home/Home.tsx
@@ -3,11 +3,10 @@ import React, { useState } from "react";
 
 import ProtectedContentContainer from "@/components/features/protected/ProtectedContentContainer";
 import HomeStatus from "@/components/features/protected/home/homeStatus/HomeStatus";
-import DashboardChatbot from "@/components/features/protected/dashboard/chatbot/DashboardChatbot";
-import { Card } from "@/components/ui/card";
 import HomeTopScore from "@/components/features/protected/home/homeTopScore/HomeTopScore";
 import SymbolModal from "@/components/common/SymbolModal";
 import { useAccountType } from "@/lib/contexts/AccountTypeContext";
+import HomeNewsAI from "./homeNewsAI/HomeNewsAI";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -24,6 +23,7 @@ export default function Home() {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
           <div className="lg:col-span-3">
             <HomeStatus />
+            <HomeNewsAI/>
           </div>
           {/* <div className="lg:col-span-1 [&_.dashboard-chatbot-card]:!h-[500px] [&_.dashboard-chatbot-card]:sm:!h-[550px] [&_.dashboard-chatbot-card]:md:!h-[500px] [&_.dashboard-chatbot-card]:lg:!h-[500px] [&_.dashboard-chatbot-card]:xl:!h-[500px] [&_.dashboard-chatbot-card]:!overflow-hidden [&_.dashboard-chatbot-card_.flex-1]:!overflow-y-auto [&_.dashboard-chatbot-card_.flex-1]:!scrollbar-hide [&_.dashboard-chatbot-card_.flex-1]:!min-h-0 [&_.dashboard-chatbot-card_.flex-1]:!max-h-none">
             <DashboardChatbot />
--- src/components/features/protected/home/homeNewsAI/HomeNewsAI.tsx
+++ src/components/features/protected/home/homeNewsAI/HomeNewsAI.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import HomeAI from "@/components/features/protected/home/homeNewsAI/HomeAI";
 import HomeNews from "@/components/features/protected/home/homeNewsAI/HomeNews";
 
 export default function HomeNewsAI() {
   return (
     <div className="flex flex-col db:flex-row items-start gap-5 self-stretch">
       <HomeNews />
-      <HomeAI />
     </div>
   );
 }
